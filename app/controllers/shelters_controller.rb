6b0fe2825e2ed9cb8afa61deddb65e8d73ce8cbf
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-05 14:53:42 -0600

    add destroy method with redirect to index

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -19,6 +19,28 @@ class SheltersController < ApplicationController
     redirect_to '/shelters'
   end
 
+  def edit
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def update
+    shelter = Shelter.find(params[:id])
+    shelter.update({
+      name: params[:shelter][:name],
+      address: params[:shelter][:address],
+      city: params[:shelter][:city],
+      state: params[:shelter][:state],
+      zip: params[:shelter][:zip]
+      })
+    shelter.save
+    redirect_to "/shelters/#{shelter.id}"
+  end
+
+  def destroy
+    Shelter.destroy(params[:id])
+    redirect_to '/shelters'
+  end
+
   # private
   # def shelter_params
   #   params.permit(:name)
