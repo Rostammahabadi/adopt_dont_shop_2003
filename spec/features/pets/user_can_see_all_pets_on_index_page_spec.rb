67b5cc34a4127fd6fc04e89667a0b068b5cd4c1a
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-07 12:42:14 -0600

    mass commit to switch to branching

---
+++ spec/features/pets/user_can_see_all_pets_on_index_page_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "pets index page", type: :feature do
+  it "can see all pets titles" do
+    binding.pry
+    shelter1 = Shelter.create(
+      name: "Puppies",
+      address: "123 west street",
+      city: "Scottsdale",
+      state: "AZ",
+      zip: 85257
+      )
+
+    pet1 = shelter1.pets.create(
+    image: "post",
+    name: "Adeline",
+    approximate_age: 1,
+    sex: "female",
+    shelter_id: 14
+    )
+
+    visit "/pets"
+
+    expect(page).to have_content(pet1.image)
+    expect(page).to have_content(pet1.name)
+    expect(page).to have_content(pet1.approximate_age)
+    expect(page).to have_content(pet1.sex)
+    expect(page).to have_content(pet1.shelter)
+  end
+end
