b854816ed482946bd94ff610b112b72a1c36f380
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-07 12:39:55 -0600

    add create and new method

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,4 +2,24 @@ class PetsController < ApplicationController
   def index
     @pets = Pet.all
   end
+
+  def show
+    @pet = Pet.find(params[:id])
+  end
+
+  def new
+    @shelter = Shelter.find(params[:shelter_id])
+  end
+
+  def create
+    shetler = Shelter.find(params[:shelter_id])
+    shelter.pets.create(pet_params)
+  end
+
+  private
+
+  def pet_params
+    params.permit(:image,:name,:approximate_age,:sex,:description)
+  end
+
 end
