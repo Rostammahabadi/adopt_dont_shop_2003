f86a3d2380dc793a2a934cbb24249083fc25fe89
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-08 11:46:07 -0600

    teset for index page for pets

--- app/controllers/shelter_pets_controller.rb
+++ app/controllers/shelter_pets_controller.rb
@@ -1,5 +1,7 @@
 class ShelterPetsController < ApplicationController
   def index
+    binding.pry
+    @param = params[:shelter_id]
     @pets = Pet.where("shelter_id = #{params[:shelter_id]}")
   end
 
