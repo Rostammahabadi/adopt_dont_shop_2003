28ecab00a65a8eccc2d469aeb737b6983dd6163e
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-09 17:34:07 -0600

    update routes to be only from two controllers (pets and shelters)

--- config/routes.rb
+++ config/routes.rb
@@ -16,11 +16,12 @@ Rails.application.routes.draw do
 
   get '/pets', to: 'pets#index'
 
-  get '/pets/:id', to: 'pets#show'
+  get '/shelters/:shelter_id/pets/new', to: 'pets#new'
+  post '/shelters/:shelter_id/pets', to: 'pets#create'
 
-  get '/shelters/:shelter_id/pets', to: 'shelter_pets#index'
-  get '/shelters/:shelter_id/pets/new', to: 'shelter_pets#new'
-  post '/shelters/:shelter_id/pets', to: 'shelter_pets#create'
+  get '/shelters/:shelter_id/pets', to: 'pets#shelter_index'
+  
+  get '/pets/:id', to: 'pets#show'
 
   get '/pets/:id/edit', to: 'pets#edit'
   patch '/pets/:id', to: 'pets#update'
