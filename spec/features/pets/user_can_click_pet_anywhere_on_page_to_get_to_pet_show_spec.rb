8b506a682edaeddf9ea66949742cbe3315ab2957
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-09 20:12:06 -0600

    test the user can click the pet through the shelter show page and the pets index page

---
+++ spec/features/pets/user_can_click_pet_anywhere_on_page_to_get_to_pet_show_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe "Pet Links", type: :feature do
+  it "can click pets on shelter show to see that pet's show page" do
+    shelter1 = Shelter.create(
+      name: "Puppies",
+      address: "123 west street",
+      city: "Scottsdale",
+      state: "AZ",
+      zip: 85257
+      )
+
+    shelter2 = Shelter.create(
+      name: "Paws",
+      address: "989 west palms",
+      city: "Denver",
+      state: "CO",
+      zip: 80202
+      )
+
+    pet1 = shelter1.pets.create(
+    image: "post",
+    name: "Adeline",
+    approximate_age: 1,
+    sex: "female"
+    )
+
+    pet2 = shelter2.pets.create(
+    image: "image",
+    name: "Joshua",
+    approximate_age: 12,
+    sex: "Male"
+    )
+
+    visit "/shelters/#{shelter1.id}"
+
+    has_link?("Adeline")
+    click_link("Adeline")
+
+    expect(current_path).to eq("/pets/#{pet1.id}")
+  end
+  it "can click on pet through the pets page to see pet show page" do
+    shelter1 = Shelter.create(
+      name: "Puppies",
+      address: "123 west street",
+      city: "Scottsdale",
+      state: "AZ",
+      zip: 85257
+      )
+
+    pet1 = shelter1.pets.create(
+    image: "post",
+    name: "Adeline",
+    approximate_age: 1,
+    sex: "female"
+    )
+
+    visit("/pets")
+    has_link?("Adeline")
+    click_link("Adeline")
+
+    expect(current_path).to eq("/pets/#{pet1.id}")
+  end
+end
