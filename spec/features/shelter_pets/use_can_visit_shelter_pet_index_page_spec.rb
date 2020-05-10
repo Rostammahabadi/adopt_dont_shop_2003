7510def42a61ec44d032d6b69d59ea32d81c9a7a
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-10 10:41:36 -0600

    added shetler_pets test for pet index page

---
+++ spec/features/shelter_pets/use_can_visit_shelter_pet_index_page_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe "shelter pets index page", type: :feature do
+  it "can see all pets that can be adopted from that shelter with that shelter id" do
+    shelter1 = Shelter.create(
+      name: "Puppies",
+      address: "123 west street",
+      city: "Scottsdale",
+      state: "AZ",
+      zip: 85257
+      )
+
+    shelter2 = Shelter.create(
+      name: "Paws",
+      address: "989 west palms",
+      city: "Denver",
+      state: "CO",
+      zip: 80202
+      )
+
+    pet1 = shelter1.pets.create(
+    image: "post",
+    name: "Adeline",
+    approximate_age: 1,
+    sex: "female"
+    )
+
+    pet2 = shelter2.pets.create(
+    image: "image",
+    name: "Joshua",
+    approximate_age: 12,
+    sex: "Male"
+    )
+
+    visit "/shelters/#{shelter1.id}/pets"
+
+    expect(page).to have_content(pet1.image)
+    expect(page).to have_content(pet1.name)
+    expect(page).to have_content(pet1.approximate_age)
+    expect(page).to have_content(pet1.sex)
+
+  end
+end
