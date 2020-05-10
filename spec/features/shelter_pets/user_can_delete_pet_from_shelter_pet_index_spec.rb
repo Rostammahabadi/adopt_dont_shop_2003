0e9f8476271dee916e35a2d4610760762226aab9
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-10 11:51:06 -0600

    add test for delete button next to each pet

---
+++ spec/features/shelter_pets/user_can_delete_pet_from_shelter_pet_index_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe "Pet Delete From Shelter Pets Index Page", type: :feature do
+  it "user can delete pet from shelter pets index page" do
+    shelter1 = Shelter.create(
+      name: "Puppies",
+      address: "123 west street",
+      city: "Scottsdale",
+      state: "AZ",
+      zip: 85257
+      )
+
+    shelter2 = Shelter.create(
+      name: "Paws",
+      address: "989 west palms",
+      city: "Denver",
+      state: "CO",
+      zip: 80202
+      )
+
+    pet1 = shelter1.pets.create(
+    image: "post",
+    name: "Adeline",
+    approximate_age: 1,
+    sex: "female"
+    )
+
+    pet2 = shelter2.pets.create(
+    image: "image",
+    name: "Joshua",
+    approximate_age: 12,
+    sex: "Male"
+    )
+
+    visit "/shelters/#{shelter1.id}/pets"
+    has_link?("Delete Pet")
+    click_link("Delete Pet", match: :first)
+
+    expect(current_path).to eq("/pets")
+    expect(page).to_not have_content(pet1)
+    expect(page).to have_content(pet2.name)
+  end
+end
