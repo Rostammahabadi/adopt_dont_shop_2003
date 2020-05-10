c16aba4beb8069f6996d7d3ee6ba773f22f23106
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-10 10:38:28 -0600

    add column for adoptable_status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200506212225) do
+ActiveRecord::Schema.define(version: 20200510160319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20200506212225) do
     t.string "sex"
     t.string "description"
     t.bigint "shelter_id"
+    t.string "adoptable_status"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
